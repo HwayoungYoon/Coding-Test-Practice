c02909edc616205f0d5d86a512c7a8c343cbb590
Hwayoung Yoon <ghkdud204@gmail.com>
2021-07-10 13:58:35 +0900

    Add files via upload

---
+++ python basic/수행 시간 측정 코드.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+프로그램 수행 시간 측정 소스코드
+"""
+
+# 측정 시작
+import time
+start_time = time.time()
+
+# 수행 시간을 측정할 프로그램 코드
+
+# 측정 종료
+end_time = time.time()
+
+# 수행 시간 출력
+print("time :", end_time - start_time)
