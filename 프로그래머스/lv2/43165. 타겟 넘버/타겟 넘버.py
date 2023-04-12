edd65b4a0f30d24459c55753852a10cfd4d5e3e4
Hwayoung Yoon <hwayoung.yoon.00@gmail.com>
2023-04-12 22:24:34 +0900

    [level 2] Title: 타겟 넘버, Time: 2.52 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,17 +1,15 @@
 from itertools import combinations
 def solution(numbers, target):
+    
     n = (sum(numbers)-target)/2
-    answer = 0
     num = [x for x in numbers if x <= n]
     x = [num]
     for i in range(1, len(num)):
         x += combinations(num,i)
+    
+    answer = 0
     for i in x:
         if sum(i) == n:
             answer += 1
+    
     return answer
-
-'''
-numbers 합에서 target 뺀 값을 2로 나눈 값이 n이라고 하면
-numbers 중 일부를 더해 n으로 만들 수 있는 경우의 수가 답
-'''
