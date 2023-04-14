34958b0f56b24c55601e348928694d1f83eb4ed6
Hwayoung Yoon <hwayoung.yoon.00@gmail.com>
2023-04-14 15:46:43 +0900

    [level 2] Title: 올바른 괄호, Time: 12.75 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,6 +1,25 @@
 def solution(s):
     stack = []
     for i in s:
+        if i == '(':
+            stack.append(i)
+        if i == ')':
+            if len(stack) == 0:
+                return False
+            elif stack[-1] == '(':
+                stack.pop()
+            elif stack[-1] == ')':
+                stack.append(i)
+    if len(stack) == 0:
+        return True
+    else:
+        return False
+
+'''
+# 첫 번째 풀이
+def solution(s):
+    stack = []
+    for i in s:
         if len(stack) == 0 and i == ')':
             return False
         if i == '(':
@@ -8,3 +27,4 @@ def solution(s):
         if i == ')' and stack[-1] == '(':
             stack.pop()
     return False if len(stack) != 0 else True
+'''
